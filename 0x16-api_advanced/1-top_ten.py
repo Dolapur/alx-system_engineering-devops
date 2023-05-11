ccfdc1eb51bd95e88ea2d39e8202cea3f5a21bfd
Dolapo Adegoju <tichiegoju@gmail.com>
2023-05-11 15:04:17 -0700

    Done

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This helps us prints"""
+import requests
+
+
+def top_ten(subreddit):
+    """Print the top 10 hottest posts on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "Dear Dolapo v1.0.0"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        print("None")
+        return
+    results = response.json()["data"]["results"]
+        for title in results:
+            print(title["data"]["title"])
